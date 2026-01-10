bd2424ffbc6bbbd764109d442348c78b9ee45844
Zuggy84 <mitch.bateup@education.nsw.gov.au>
2026-01-10 12:31:46 +1100

    Added .py App File, Fixed Hyper Links to Accomodate to .py App File, Added Title to Reviews Page on .html File, Fixed Links to .css Files to Accomodate to .py App File, Changed Folder Names to Accomodate to .py App File.

---
+++ Taste Tracker/Taste Tracker.py
@@ -0,0 +1,59 @@
+from flask import Flask
+app=Flask(__name__)
+@app.route('/')
+def home():
+    Taste_Tracker_Home_Page='''
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <link rel="stylesheet" type="text/css" href="/static/css/Taste Tracker Home Page.css">
+            <title>Taste Tracker Home Page</title>
+        </head>
+        <body>
+            <hr>
+            <div style="margin: 20px; border: 2px solid grey; padding: 2px">
+                <div style="border: 2px solid grey; padding: 10px">
+                    <h1>Welcome To Taste Tracker!</h1>
+                    <h2>View And Share Reviews About Restaurants <span style="color: red">A<span style="color: orange">L<span style="color: yellow">L <span style="color: green">A<span style="color: blue">C<span style="color: purple">R<span style="color: red">O<span style="color: orange">S<span style="color: yellow">S <span style="color: green">T<span style="color: blue">O<span style="color: purple">W<span style="color: red">N<span style="color: orange">!</h2>
+                </div>
+            </div>
+            <hr>
+            <div style="margin: 20px; border: 2px solid grey; padding: 2px">
+                <div style="border:2px solid grey; padding: 10px">
+                    <h2>Navigate Taste Tracker:</h2>
+                    <br>
+                    <div class="container">
+                        <a href="/reviews">Reviews Page</a>
+                    </div>
+                    <hr>
+                    <h1>OR</h1>
+                    <hr>
+                </div>
+            </div>
+            <hr>
+        </body>
+    </html>
+    '''
+    return Taste_Tracker_Home_Page
+@app.route('/reviews')
+def reviews():
+    Taste_Tracker_Reviews_Page='''
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <link rel="stylesheet" type="text/css" href="/static/css/Taste Tracker Reviews Page.css">
+            <title>Taste Tracker Reviews Page</title>
+        </head>
+        <body>
+            <hr>
+            <div style="margin: 20px; border: 2px solid grey; padding: 2px">
+                <div style="border: 2px solid grey; padding: 10px">
+                    <h1>Taste Tracker Reviews Page</h1>
+                </div>
+            </div>
+            <hr>
+        </body>
+    </html>
+    '''
+    return Taste_Tracker_Reviews_Page
+app.run(debug=True, reloader_type='stat', port=5000)
