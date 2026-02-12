467714eaf3167b2206c2a45da30a12316c9af2eb
Zuggy84 <mitch.bateup@education.nsw.gov.au>
2026-02-12 22:53:20 +1100

    Edited Credits and Reviews Page with Adding hr's and Hyperlinks back to Home Page as well as Other Changes, Create hr Style in .css File, Added and Removed Indentation Throughout All Webpages, Added an if Statement on the Last Line of the .py File, Added Sign Up and Log In Pages, Edited .db File and Made Sure all Required Tables Exist, Added Sign Up Functionality, Added Add Review Functionality, Changed and Added some title Tags on All Webpages, Added Log In Functionality, Added if Statements for user_logged_in Variable to Ensure Everything Flows Correcctly, Added Invalid Log In Functionality in case Credentials are Incorrect, Instance Folder Created due to Interactions with Database.

--- Taste Tracker/app.py
+++ Taste Tracker/app.py
@@ -1,17 +1,70 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from sqlalchemy import create_engine, text
+from werkzeug.security import generate_password_hash, check_password_hash
 app=Flask(__name__)
 engine=create_engine('sqlite:///Taste Tracker.db')
 connection=engine.connect()
+user_logged_in=False
 @app.route('/')
 def home():
-    return render_template('Taste Tracker Home Page.html')
-@app.route('/reviews')
+    if user_logged_in==True:
+        return render_template('Taste Tracker Home Page.html', user_logged_in=user_logged_in)
+    else:
+        return render_template('Taste Tracker Home Page.html')
+@app.route('/reviews', methods=['GET', 'POST'])
 def reviews():
-    query=text('SELECT * FROM reviews')
-    result=connection.execute(query).fetchall()
-    return render_template('Taste Tracker Reviews Page.html', reviews=result)
+    if request.method=='POST':
+        restaurant_name=request.form['restaurant_name']
+        cuisine_type=request.form['cuisine_type']
+        restaurant_rating=request.form['restaurant_rating']
+        restaurant_review_text=request.form['restaurant_review_text']
+        query=text('INSERT INTO reviews (restaurant_name, cuisine_type, restaurant_rating, restaurant_review_text) VALUES (:restaurant_name, :cuisine_type, :restaurant_rating, :restaurant_review_text);')
+        connection.execute(query, {'restaurant_name': restaurant_name, 'cuisine_type': cuisine_type, 'restaurant_rating': restaurant_rating, 'restaurant_review_text': restaurant_review_text})
+        connection.commit()
+        query=text('SELECT * FROM reviews;')
+        result=connection.execute(query).fetchall()
+        user_logged_in=True
+        return render_template('Taste Tracker Reviews Page.html', reviews=result, user_logged_in=user_logged_in)
+    else:
+        query=text('SELECT * FROM reviews;')
+        result=connection.execute(query).fetchall()
+        return render_template('Taste Tracker Reviews Page.html', reviews=result)
 @app.route('/credits')
 def credits():
-    return render_template('Taste Tracker Credits Page.html')
-app.run(debug=True, reloader_type='stat', port=5000)
+    if user_logged_in==True:
+        return render_template('Taste Tracker Credits Page.html', user_logged_in=user_logged_in)
+    else:
+        return render_template('Taste Tracker Credits Page.html')
+@app.route('/signUp', methods=['GET', 'POST'])
+def signUp():
+    if request.method=='POST':
+        user_name=request.form['user_name']
+        user_email=request.form['user_email']
+        user_password=request.form['user_password']
+        user_password_hash=generate_password_hash(user_password)
+        query=text('INSERT INTO users (user_name, user_email, user_password, user_password_hash) VALUES (:user_name, :user_email, :user_password, :user_password_hash);')
+        connection.execute(query, {'user_name': user_name, 'user_email': user_email, 'user_password': user_password, 'user_password_hash': user_password_hash})
+        connection.commit()
+        user_logged_in=True
+        return render_template('Taste Tracker Reviews Page.html', user_logged_in=user_logged_in)
+    else:
+        return render_template('Taste Tracker Sign Up Page.html')
+@app.route('/logIn', methods=['GET', 'POST'])
+def logIn():
+    if request.method=='POST':
+        user_email=request.form['user_email']
+        user_password=request.form['user_password']
+        query=text('SELECT user_password FROM users WHERE user_email = :user_email;')
+        result=connection.execute(query, {'user_email': user_email}).fetchone()
+        if result:
+            fetched_password=result[0]
+            if fetched_password==user_password:
+                user_logged_in=True
+                return render_template('Taste Tracker Reviews Page.html', user_logged_in=user_logged_in)
+            else:
+                user_not_logged_in=True
+                return render_template('Taste Tracker Log In Page.html', user_not_logged_in=user_not_logged_in)  
+    else:
+        return render_template('Taste Tracker Log In Page.html')
+if __name__=='__main__':
+    app.run(debug=True, reloader_type='stat', port=5000)
